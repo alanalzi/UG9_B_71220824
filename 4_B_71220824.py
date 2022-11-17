8e60a4fd718d9027f9c47c0741837d9428f4a737
alanalzi <117884402+alanalzi@users.noreply.github.com>
2022-11-17 15:12:28 +0700

    Add files via upload

--- 4_B_71220824.py
+++ 4_B_71220824.py
@@ -2,9 +2,33 @@ print ("<<<<<<<<<< Adventure of Alan >>>>>>>>>>")
 
 print ("Welcome to the Adventure of Alan!")
 
-gender = input("kamu pria atau wanita (P/W): ")
+p1 = input("kamu pria atau wanita (P/W): \n>>> ")
+if p1== 'P':
+    print ("Hello Bro")
+else:
+    print ("Hai Manies")
+print("\n\n")
+p2 = input("Udah punya pacar kah? (punya/belum) \n>>> ") 
+if p2== 'punya':
+    print ("Putusin aja, cari yang lain")
+else:
+    print ("yhahahaha ga laku")
+print("\n\n")
+p3 = input ("pernah ketabrak truck ga? (ga/iya) \n>>> ")
+if p3== 'ga':
+    print ("wah lemah")
+else:
+    p4 = input("kok bisa jawab pertanyaan ini? (immortal/revive) \n>>> ")
+    if p4== 'immortal':
+        print ("wah keren \n\n")
+    else:
+        print ("wah keren juga")
+print("\n\n")
+p5 = input ("Mau kah kamu berteman dengan aku? (iya/ga) \n>>> ")
+if p5== 'iya':
+    print ("yaudah kita berteman nih")
+else:
+    print ("yaudah sana, dasar sombong")
+    
+           
 
-print ("")
-print ("")
-print ("")
-input ("suatu hari ada orang bernama alan, dia kuliah di UKDW")
